5baf2a6eae40da50864aaa286d1b919834a0cbd6
zihadulislam2 <zihadul708@gmail.com>
2024-10-30 13:15:53 +0600

    change cookie logic to session storage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { json, Route, Routes } from 'react-router-dom'
 import AuthLayout from './components/auth/layout'
 import AuthLogin from './pages/auth/login'
 import AuthRegister from './pages/auth/register'
@@ -29,7 +29,8 @@ function App() {
   const dispatch = useDispatch()
 
   useEffect(() => {
-    dispatch(checkAuth())
+    const token = JSON.parse(sessionStorage.getItem('token'))
+    dispatch(checkAuth(token))
   }, [dispatch])
 
   if (isLoading) return <Skeleton className="w-[800] bg-black h-[600px]" />
