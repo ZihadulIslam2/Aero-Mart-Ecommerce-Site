4eab7b9e332f413bc574e287e2fd05ff2aa6de93
zihadulislam2 <zihadul708@gmail.com>
2025-12-19 11:40:39 +0600

    add ChatBot component with toggle functionality and styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,6 +22,7 @@ import { Skeleton } from '@/components/ui/skeleton'
 import PaypalReturnPage from './pages/shopping-view/paypal-return'
 import PaymentSuccessPage from './pages/shopping-view/payment-success'
 import SearchProducts from './pages/shopping-view/search'
+import ChatBot from './components/ChatBot/ChatBot'
 
 function App() {
   const { user, isAuthenticated, isLoading } = useSelector(
@@ -73,6 +74,8 @@ function App() {
           <Route path="orders" element={<AdminOrders />} />
           <Route path="features" element={<AdminFeatures />} />
         </Route>
+        {/* ChatBoot components */}
+
         <Route
           path="/shop"
           element={
@@ -92,6 +95,7 @@ function App() {
         <Route path="/unauth-page" element={<UnauthPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <ChatBot />
     </div>
   )
 }
